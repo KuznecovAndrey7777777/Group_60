28d754045f5a9db6206eaf435c87dbe90c37d0fc
KuznecovAndrey7777777 <107943133+KuznecovAndrey7777777@users.noreply.github.com>
2023-11-26 16:33:07 +0200

    save 2611231633

--- home_work7/js/01-gallery.js
+++ home_work7/js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+    const galleryItem = document.createElement('div');
+    galleryItem.className = 'gallery__item';
+
+    const galleryLink = document.createElement('a');
+    galleryLink.className = 'gallery__link';
+    galleryLink.href = original;
+
+    const galleryImage = document.createElement('img');
+    galleryImage.className = 'gallery__image';
+    galleryImage.src = preview;
+    galleryImage.setAttribute('data-source', original);
+    galleryImage.alt = description;
+
+    galleryLink.appendChild(galleryImage);
+    galleryItem.appendChild(galleryLink);
+
+    return galleryItem;
+}
+
+const items = galleryItems.map(createGalleryItem);
+gallery.append(...items);
+
+gallery.addEventListener('click', (event) => {
+    event.preventDefault();
+    const target = event.target;
+
+    if (target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(`<img src=${target.dataset.source}>`, {
+        onShow: () => document.addEventListener('keydown', onModalCloseToEscape),
+        onClose: () => document.removeEventListener('keydown', onModalCloseToEscape),
+    });
+
+    instance.show();
+
+    function onModalCloseToEscape(event) {
+        if (event.code === 'Escape') {
+            instance.close();
+        }
+    }
+});
--- home_work7/js/02-lightbox.js
+++ home_work7/js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const items = [];
+
+galleryItems.forEach((element) => {
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.href = element.original;
+
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.src = element.preview;
+    galleryImage.alt = element.description;
+
+    galleryLink.appendChild(galleryImage);
+    galleryItem.appendChild(galleryLink);
+    items.push(galleryItem);
+});
+
+gallery.append(...items);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
